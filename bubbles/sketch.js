a3eccd439cd68a466d72671dd0699eece6c5c749
OfficialPedroDimas <p.dimas@campus.fct.unl.pt>
2018-09-20 20:45:54 +0100

    patch

--- bubbles/sketch.js
+++ bubbles/sketch.js
@@ -1,17 +1,19 @@
 var points = [];
 var big = [];
+var ammount;
 
 function setup() {
     createCanvas(400, 400);
     stroke(255);
     fill(50);
+    slider = createSlider(0, 10, 1, 0.01);
 }
 
 function drawAndBig(){
     for (let i = 0; i < points.length; i++) {
         if(!(big[i] > 100)){
             ellipse(points[i].x, points[i].y, big[i], big[i]);
-        big[i]+=2;
+        big[i]+=ammount;
         }
     
     }
@@ -21,6 +23,7 @@ function drawAndBig(){
 
 function draw() {
     background(50);
+    ammount = slider.value();
     var pos = createVector(random(0, width), random(0,height));
     points.push(pos);
     big.push(10);
